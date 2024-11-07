b6f5b1a6a80b049fc3c80f60b396e62e96142b58
Esteban Thiriet <esteban.thiriet3@etu.univ-lorraine.fr>
2024-11-07 12:29:17 +0100

    ajout Spectacle + InvalidPropertyNameException

--- src/classes/festival/Spectacle.php
+++ src/classes/festival/Spectacle.php
@@ -1,2 +1,33 @@
 <?php
 
+namespace iutnc\nrv\festival;
+
+use iutnc\deefy\exception\InvalidPropertyNameException;
+
+class Spectacle
+{
+    protected string $nom;
+    protected string $horaireDebut;
+    protected string $horaireFin;
+    protected string $style;
+    protected string $description;
+
+    public function __construct($nom, $horaireDebut, $horaireFin, $style = "Inconnu", $description = "Aucune description")
+    {
+        $this->nom = $nom;
+        $this->horaireDebut = $horaireDebut;
+        $this->horaireFin = $horaireFin;
+        $this->style = $style;
+        $this->description = $description;
+    }
+
+    public function __get(string $property): mixed
+    {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        } else {
+            throw new InvalidPropertyNameException($property);
+        }
+    }
+
+}
