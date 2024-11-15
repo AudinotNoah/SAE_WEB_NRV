e328ef3ad838a761f6ef1f98edb6192f556c6794
Raouf-blip <abdoul-raouf.temirov2@etu.univ-lorraine.fr>
2024-11-15 14:44:03 +0100

    background pour accueil

--- src/classes/action/DefaultAction.php
+++ src/classes/action/DefaultAction.php
@@ -7,9 +7,21 @@ class DefaultAction extends Action {
     protected function get(): string
     {
         return "
-        <div class='background-image content'>
+        <div class='content has-text-centered' style='
+            background-image: url(\"src/assets/images/siteImage/backAccueil.jpg\");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        '>
             <h2 class='title is-2 has-text-black'>Bienvenue au Festival NRV !</h2>
-            <p class='is-size-5 has-text-black'>Le festival NRV à Nancy est l'événement musical de l'année. Rejoignez-nous pour une expérience inoubliable !</p>
+            <p class='is-size-5 has-text-black'>
+                Le festival NRV à Nancy est l'événement musical de l'année. Rejoignez-nous pour une expérience inoubliable !
+            </p>
         </div>";
     }
 }
