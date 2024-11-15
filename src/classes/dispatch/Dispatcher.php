d31f8f06bd7c46edffcb501635ac5037cc04cb71
NaasaX <126869845+NaasaX@users.noreply.github.com>
2024-11-15 19:12:33 +0100

    Update Dispatcher.php

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -160,7 +160,6 @@ HTML;
         <head>
             <meta charset='UTF-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <link rel='stylesheet' href='src/assets/css/style.css'>
             <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>
             <title>Festival de Musique - Administration</title>
         </head>
